7421673839d84c3a946044c41517da012ddda345
FarhanAlmasyhur <farhan.almasyhur@gmail.com>
2023-06-25 15:46:22 +0800

    feat: setup xcode project

---
+++ Memoria/MemoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriaApp.swift
+//  Memoria
+//
+//  Created by Muhammad Farhan Almasyhur on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

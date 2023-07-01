b60b60876ab6c1244c1be319bd3949365689abd3
FarhanAlmasyhur <farhan.almasyhur@gmail.com>
2023-07-01 17:43:20 +0800

    fix: copywriting home

--- Memoria/Module/Home/HomeView.swift
+++ Memoria/Module/Home/HomeView.swift
@@ -20,16 +20,21 @@ struct HomeView: View {
                         Image("img_scanqr")
                             .resizable()
                             .scaledToFit()
-                            .frame(width: geometry.size.width * 0.9)
                             .padding(.top, 30)
                         VStack {
                             Image("img_phone_scanqr")
                                 .resizable()
                                 .scaledToFit()
                                 .frame(height: geometry.size.height * 0.4)
-                            Text("")
+                            Text("With a ***QR code from Memoria***, guests can conveniently capture their favorite moments.")
+                                .font(.custom("Inter", fixedSize: 14))
+                                .fontWeight(.regular)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.white)
+                                .padding(.top, 10)
                         }
-                    }.padding(.bottom, 5)
+                    }.frame(width: geometry.size.width * 0.9)
+                    .padding(.bottom, 5)
                     LargeButtonImage(title: "Join by Scan QR", image: "qrcode", backgroundColor: .purpleButton, foregroundColor: .white) {
                         withAnimation(.spring()) {
                             appStateNavigation.showJoinByQR = true
